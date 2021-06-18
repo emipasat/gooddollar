716fa93eeb98fe51c181540f90ebe380b4039df0
Emanuel Pasat <emipasat@yahoo.com>
2021-06-18 12:47:40 +0300

    cors

--- server/apiServer.js
+++ server/apiServer.js
@@ -31,10 +31,20 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
   }
 
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  //res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept"
+  // );
   next();
 });
 
